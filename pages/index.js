04d64850e0734d12d91678c70bbb8fe70daca51a
Taeyoung Jang <xoxodudwkd@gmail.com>
2022-01-28 19:52:10 +0900

    docs: #1.2 Static Pre Rendering

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,12 @@
+import { useState } from 'react';
+
 export default function Home() {
-  return 'hi';
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <div>
+      <h1>Hello World! {counter}</h1>
+      <button onClick={() => setCounter((prev) => prev + 1)}>+</button>
+    </div>
+  );
 }
